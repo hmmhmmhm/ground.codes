9d0212acb29e064be7777d339fb2a24b67cf4e60
hmmhmmhm <hmmhmmhm@naver.com>
2025-03-12 00:47:54 +0900

    feat: (geoint) Make public package

---
+++ packages/geoint/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: {
+    compilerOptions: {
+      resolveJsonModule: false,
+    },
+  },
+  splitting: true,
+  sourcemap: false,
+  clean: true,
+});
