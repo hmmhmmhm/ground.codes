22d2a5e586e92e4faaf9ee56ac83d8bc57f4e296
hmmhmmhm/ <hmmhmmhm@naver.com>
2025-03-14 07:53:07 +0900

    fix: (web) Disable eslint in build time

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,6 +10,11 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
       process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   trailingSlash: true,
 };
 
