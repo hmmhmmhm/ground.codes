8e3659a4f1e1c81a3fe08f101036ac250fe55259
hmmhmmhm <hmmhmmhm@naver.com>
2025-03-13 17:26:45 +0900

    fix: (web) Debug env

--- apps/web/components/google-map/hooks/use-map-container.ts
+++ apps/web/components/google-map/hooks/use-map-container.ts
@@ -20,6 +20,10 @@ export const useMapContainer = () => {
   const { getUserLanguage } = useLanguage();
 
   // Load Google Maps API
+  console.log(
+    "process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY",
+    process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+  );
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
