fbe05584d11bee7b84ed542d839c9282304600e5
hmmhmmhm <hmmhmmhm@naver.com>
2025-03-20 23:28:19 +0900

    chore: (web) Modify mapId

--- apps/web/components/google-map/hooks/use-map-container.ts
+++ apps/web/components/google-map/hooks/use-map-container.ts
@@ -191,7 +191,7 @@ export const useMapContainer = () => {
           map.setOptions({
             styles: googleMapDarkTheme,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
-            mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_ROADMAP_ID || "",
+            mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_ROADMAP_ID,
           });
         } else {
           // For satellite view, use empty styles array and explicitly set satellite map type
@@ -419,7 +419,7 @@ export const useMapContainer = () => {
         mapInstance.setOptions({
           styles: googleMapDarkTheme,
           mapTypeId: google.maps.MapTypeId.ROADMAP,
-          mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_ROADMAP_ID || "",
+          mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_ROADMAP_ID,
         });
       } else {
         mapInstance.setOptions({
