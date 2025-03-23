82a36b745e80c246e57171d3b5b63f2b330a7549
hmmhmmhm <hmmhmmhm@naver.com>
2025-03-23 12:30:32 +0900

    chore: (web) Disable iOS Rubber band scrolling prevent hook

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import GoogleMap from "@/components/google-map";
-import { useDisableRubberBandEffect } from "@/hooks/use-disable-rubber-band";
 
 export default function Home() {
-  // Apply the hook to disable iOS rubber band scrolling effect
-  useDisableRubberBandEffect();
-  
   return (
     <div className="absolute min-h-screen-safe bg-black w-full h-full select-none overflow-hidden">
       <GoogleMap />
